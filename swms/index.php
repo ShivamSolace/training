8700c2e0aecbc1258514cc1146033a35178b4757
Shivam Yadav <124343543+ShivamSolace@users.noreply.github.com>
2023-03-13 18:48:16 +0530

    Update index.php

--- swms/index.php
+++ swms/index.php
@@ -1,39 +1,48 @@
 <?php
 include('./public/header.php');
 include('./public/meta.php');
-if(isset($_POST['submit'])){
-  $userName= $_POST['eMail'];
-  $userPassword= $_POST['password'];
-  $data= $auth->login($userName,$userPassword);
-  echo "".$data;
+$displayError = "";
+
+if (isset($_POST['submit'])) {
+  $userMail = $_POST['eMail'];
+  $userPassword = $_POST['password'];
+  $userFirstName = $_POST[`'firstName`];
+  $displayError = $auth->login($userMail, $userPassword);
+
+  session_start();
+
+  if ($displayError) {
+    $_SESSION['userMail'] = $userMail;
+    header("Location: ./user/admin/dashboard.php");
+  } else {
+    $displayError = "Incorrect Input";
+  }
 }
 ?>
-  
+
 <!-- body start -->
 <div class="login-form">
   <form action="index.php" method="POST">
     <h1>Login to Your Soceity</h1>
     <div class="content">
       <div class="input-field">
-        <input type="email" placeholder="Email" autocomplete="nope" name="eMail"/>
+        <input type="email" placeholder="Email" autocomplete="nope" name="eMail" required />
       </div>
       <div class="input-field">
-        <input type="password" placeholder="Password" autocomplete="new-password" name="password"/>
+        <input type="password" placeholder="Password" autocomplete="new-password" name="password" required />
       </div>
-      <a href="html/forgetPassword.html" class="link">Forgot Your Password?</a>
+      <a href="forgotpassword.php" class="link">Forgot Your Password?</a>
     </div>
     <div class="action">
-    <!-- <button><a href="html/register.html">Register</a></button> -->
-    <!-- <button type="submit" name="submit" >Sign in</button> -->
-    <button><input type="submit" name="submit"  value="Login"></button>
+      <!-- <button><a href="html/register.html">Register</a></button> -->
+      <!-- <button type="submit" name="submit" >Sign in</button> -->
+      <button><input type="submit" name="submit" value="Login"></button>
+      <?php echo $displayError ? ($displayError) : (""); ?>
     </div>
   </form>
 </div>
 <!-- body end -->
 
 <?php
-//  $userName = $_POST["eMail"];
-//  $userPassword = $_POST["password"];
-//  $auth->login($userName,$userPassword);
 include('./public/footer.php');
 ?>
